d7317b5642bbffbbe23eef0374a667765247e0a4
Steve Engledow <sengledo@amazon.co.uk>
2019-04-03 15:02:40 +0100

    Add basic graph functionality (needs a tidy)

---
+++ cmd/graph.go
@@ -0,0 +1,64 @@
+package cmd
+
+import (
+	"codecommit/builders/cfn-cli/lib"
+	"fmt"
+	"os"
+	"sort"
+
+	"github.com/awslabs/aws-cloudformation-template-formatter/parse"
+)
+
+func init() {
+	Commands["graph"] = Command{
+		Func: graphCommand,
+		Help: "Graph the dependencies between resources in a template",
+	}
+}
+
+func graphCommand(args []string) {
+	if len(args) != 1 {
+		fmt.Fprintln(os.Stderr, "Usage: cfn graph <template filename>")
+		os.Exit(1)
+	}
+
+	fileName := args[0]
+
+	input, err := parse.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	template := lib.Template(input)
+
+	graph := make(map[string]map[string]bool)
+
+	for _, dep := range template.FindDependencies() {
+		from := dep.From.String()
+		to := dep.To.String()
+
+		if _, ok := graph[from]; !ok {
+			graph[from] = make(map[string]bool)
+		}
+
+		graph[from][to] = true
+	}
+
+	froms := make([]string, 0)
+
+	for from, _ := range graph {
+		froms = append(froms, from)
+	}
+
+	sort.Strings(froms)
+
+	for _, from := range froms {
+		fmt.Println(from)
+
+		for to, _ := range graph[from] {
+			fmt.Println("  ->", to)
+		}
+
+		fmt.Println()
+	}
+}
---
+++ lib/template.go
@@ -0,0 +1,148 @@
+package lib // FIXME: Rename this
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+const pseudoParameterType = "PseudoParameter"
+
+type Template map[string]interface{}
+type tree map[string]interface{}
+
+var typeMap = map[string]string{
+	"Resources":  "Resource",
+	"Outputs":    "Output",
+	"Parameters": "Parameter",
+}
+
+var subRe = regexp.MustCompile(`\$\{([^!].+?)\}`)
+
+type Node struct {
+	Type string
+	Name string
+}
+
+type Dependency struct {
+	From Node
+	To   Node
+}
+
+func (n Node) String() string {
+	return fmt.Sprintf("%s (%s)", n.Name, n.Type)
+}
+
+func (d Dependency) String() string {
+	return fmt.Sprintf("%s -> %s", d.From, d.To)
+}
+
+func (t Template) FindDependencies() []Dependency {
+	deps := make([]Dependency, 0)
+
+	var entityTypes = make(map[string]string)
+
+	for typeName, entity := range t {
+		entityTree, ok := entity.(map[string]interface{})
+
+		if ok {
+			for entityName, _ := range entityTree {
+				entityTypes[entityName] = typeMap[typeName]
+			}
+		}
+	}
+
+	for typeKey, typeName := range typeMap {
+		// Resources
+		for name, res := range t[typeKey].(map[string]interface{}) {
+			resource := tree(res.(map[string]interface{}))
+			for to := range resource.Refs() {
+				toType, ok := entityTypes[to]
+				if !ok {
+					switch {
+					case strings.HasPrefix(to, "AWS::"):
+						toType = pseudoParameterType
+					default:
+						toType = "Unknown"
+					}
+				}
+
+				deps = append(deps, Dependency{
+					From: Node{typeName, name},
+					To:   Node{toType, to}, // FIXME: Find out what type of thing this is
+				})
+			}
+		}
+	}
+
+	return deps
+}
+
+func (t tree) Refs() chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer close(ch)
+		t.findRefs(ch)
+	}()
+
+	return ch
+}
+
+func (t tree) findRefs(ch chan string) {
+	for key, value := range t {
+		switch key {
+		case "Ref":
+			ch <- value.(string)
+		case "Fn::GetAtt":
+			switch v := value.(type) {
+			case string:
+				parts := strings.Split(v, ".")
+				ch <- parts[0]
+			case []interface{}:
+				if s, ok := v[0].(string); ok {
+					ch <- s
+				}
+			default:
+				fmt.Printf("Malformed GetAtt: %T\n", v)
+			}
+		case "Fn::Sub":
+			switch v := value.(type) {
+			case string:
+				for _, groups := range subRe.FindAllStringSubmatch(v, 1) {
+					ch <- groups[1]
+				}
+			case []interface{}:
+				if parts, ok := v[1].(tree); ok {
+					for _, part := range parts {
+						ch <- part.(string)
+					}
+				}
+			default:
+				fmt.Printf("Malformed Sub: %T\n", v)
+			}
+		default:
+			treeChan := make(chan tree)
+
+			go func() {
+				findTrees(treeChan, value)
+				close(treeChan)
+			}()
+
+			for tree := range treeChan {
+				tree.findRefs(ch)
+			}
+		}
+	}
+}
+
+func findTrees(ch chan tree, value interface{}) {
+	switch v := value.(type) {
+	case map[string]interface{}:
+		ch <- tree(v)
+	case []interface{}:
+		for _, child := range v {
+			findTrees(ch, child)
+		}
+	}
+}
