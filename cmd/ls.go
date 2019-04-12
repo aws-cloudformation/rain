ccb17dd68dc02e0c9949b13dba3d3bdee2661cbc
Steve Engledow <sengledo@amazon.co.uk>
2019-04-12 15:52:32 +0100

    Support listing stack contents

--- cmd/ls.go
+++ cmd/ls.go
@@ -2,6 +2,10 @@ package cmd
 
 import (
 	"codecommit/builders/cfn-cli/util"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
 )
 
 var liveStatuses = []string{
@@ -30,8 +34,8 @@ func init() {
 	}
 }
 
-func lsCommand(args []string) {
-	args = append([]string{
+func listStacks() {
+	cmdArgs := append([]string{
 		"cloudformation",
 		"list-stacks",
 		"--output", "table",
@@ -39,5 +43,69 @@ func lsCommand(args []string) {
 		"--stack-status-filter",
 	}, liveStatuses...)
 
-	util.RunAttached("aws", args...)
+	util.RunAttached("aws", cmdArgs...)
+}
+
+func listStack(name string) {
+	var client *cloudformation.CloudFormation
+
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		panic("Couldn't load AWS config: " + err.Error())
+	}
+	client = cloudformation.New(cfg)
+
+	// Get the stack properties
+	stackReq := client.DescribeStacksRequest(&cloudformation.DescribeStacksInput{
+		StackName: &name,
+	})
+
+	stackRes, err := stackReq.Send()
+	if err != nil {
+		panic(err)
+	}
+
+	// Get the resources
+	resourceReq := client.DescribeStackResourcesRequest(&cloudformation.DescribeStackResourcesInput{
+		StackName: &name,
+	})
+
+	resourceRes, err := resourceReq.Send()
+	if err != nil {
+		panic(err)
+	}
+
+	// Now print it!
+
+	fmt.Printf("%s:\n", name)
+	fmt.Println()
+
+	fmt.Printf("  Status: %s\n", stackRes.Stacks[0].StackStatus)
+	fmt.Println()
+
+	fmt.Println("  Parameters:")
+	for _, param := range stackRes.Stacks[0].Parameters {
+		fmt.Printf("    %s: %s\n", *param.ParameterKey, *param.ParameterValue)
+	}
+	fmt.Println()
+
+	fmt.Println("  Outputs:")
+	for _, output := range stackRes.Stacks[0].Outputs {
+		fmt.Printf("    %s: %s\n", *output.OutputKey, *output.OutputValue)
+	}
+	fmt.Println()
+
+	fmt.Println("  Resources:")
+	for _, resource := range resourceRes.StackResources {
+		fmt.Printf("    %s: %s (%s)\n", *resource.LogicalResourceId, *resource.PhysicalResourceId, *resource.ResourceType)
+	}
+	fmt.Println()
+}
+
+func lsCommand(args []string) {
+	if len(args) == 0 {
+		listStacks()
+	} else if len(args) == 1 {
+		listStack(args[0])
+	}
 }
