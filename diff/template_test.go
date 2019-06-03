949bf711ffe7e1a0ac2337a1015272ea28db8cfa
Steve Engledow <steve@offend.me.uk>
2019-06-03 17:59:52 +0100

    Test that we can successfully compare templates parsed by the parse package

---
+++ diff/template_test.go
@@ -0,0 +1,55 @@
+package diff
+
+import (
+	"testing"
+
+	"github.com/aws-cloudformation/rain/parse"
+)
+
+const a = `Parameters:
+  BucketName:
+    Type: String
+Resources:
+  Bucket:
+    Type: AWS::S3::Bucket
+  Properties:
+    BucketName: !Ref BucketName
+Outputs:
+  BucketArn:
+    Value: !GetAtt Bucket.Arn
+`
+
+const b = `Parameters:
+  BucketName:
+    Type: String
+Resources:
+  Bucket:
+    Type: AWS::S3::Bucket
+  Properties:
+    BucketName:
+      Ref: BucketName
+Outputs:
+  BucketArn:
+    Value:
+      Fn::GetAtt:
+        - Bucket
+        - Arn
+`
+
+func TestCompareTemplateIntrinsics(t *testing.T) {
+	at, err := parse.ReadString(a)
+	if err != nil {
+		t.Error(err)
+	}
+
+	bt, err := parse.ReadString(b)
+	if err != nil {
+		t.Error(err)
+	}
+
+	actual := Compare(at, bt)
+
+	if actual != Unchanged {
+		t.Errorf("Templates are not equal! %s", Format(actual))
+	}
+}
