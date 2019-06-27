5ca3dfe2b3c99250a4f5082609efa64ccf5b744c
Steve Engledow <steve@offend.me.uk>
2019-06-27 17:14:07 +0100

    Add tree command

--- cmd/tree.go
+++ cmd/tree.go
@@ -2,16 +2,71 @@ package cmd
 
 import (
 	"fmt"
+	"sort"
 
-	"github.com/aws-cloudformation/rain/format"
-	"github.com/aws-cloudformation/rain/lib"
 	"github.com/aws-cloudformation/rain/parse"
+	"github.com/aws-cloudformation/rain/template"
+	"github.com/aws-cloudformation/rain/util"
 	"github.com/spf13/cobra"
 )
 
+var allLinks = false
+
+func printLinks(links []interface{}, typeFilter string) {
+	names := make([]string, 0)
+	for _, link := range links {
+		to := link.(template.Element)
+		if to.Type == typeFilter {
+			names = append(names, to.Name)
+		}
+	}
+
+	if len(names) == 0 {
+		return
+	}
+
+	fmt.Printf("    %s:\n", typeFilter)
+	for _, name := range names {
+		fmt.Printf("      - %s\n", util.Orange(name))
+	}
+}
+
+func printGraph(graph template.Graph, typeFilter string) {
+	froms := make([]template.Element, 0)
+	fromLinks := make(map[template.Element][]interface{})
+
+	for _, item := range graph.Items() {
+		from := item.(template.Element)
+		if from.Type == typeFilter {
+			links := graph.Get(item)
+
+			if !allLinks && len(links) == 0 {
+				continue
+			}
+
+			froms = append(froms, from)
+			fromLinks[from] = links
+		}
+	}
+
+	if len(froms) == 0 {
+		return
+	}
+
+	fmt.Printf("%s:\n", typeFilter)
+
+	for _, from := range froms {
+		fmt.Printf("  %s:\n", util.Yellow(from.Name))
+		printLinks(fromLinks[from], "Parameters")
+		printLinks(fromLinks[from], "Resources")
+	}
+
+	fmt.Println()
+}
+
 var graphCmd = &cobra.Command{
 	Use:                   "tree [template]",
-	Short:                 "Finds dependencies between entities in a CloudFormation template",
+	Short:                 "Finds dependencies of resources and outputs in a CloudFormation template",
 	Long:                  "Find and display the dependencies between parameters, resources, and outputs in a CloudFormation template.",
 	Args:                  cobra.ExactArgs(1),
 	Aliases:               []string{"graph"},
@@ -24,39 +79,17 @@ var graphCmd = &cobra.Command{
 			panic(fmt.Errorf("Unable to parse template '%s': %s", fileName, err))
 		}
 
-		template := lib.Template(input)
+		t := template.Template(input)
 
-		graph := make(map[string]interface{})
+		graph := t.Graph()
+		sort.Sort(graph)
 
-		for _, dep := range template.FindDependencies() {
-			fromType := fmt.Sprintf("%ss", dep.From.Type)
-			toType := fmt.Sprintf("%ss", dep.To.Type)
-
-			fromTypeGraph, ok := graph[fromType].(map[string]interface{})
-			if !ok {
-				fromTypeGraph = make(map[string]interface{})
-				graph[fromType] = fromTypeGraph
-			}
-
-			fromNameGraph, ok := fromTypeGraph[dep.From.Name].(map[string]interface{})
-			if !ok {
-				fromNameGraph = make(map[string]interface{})
-				fromTypeGraph[dep.From.Name] = fromNameGraph
-			}
-
-			_, ok = fromNameGraph[toType].([]string)
-			if !ok {
-				fromNameGraph[toType] = make([]string, 0)
-			}
-
-			fromNameGraph[toType] = append(fromNameGraph[toType].([]string), dep.To.Name)
-		}
-
-		fmt.Println(format.Yaml(graph))
+		printGraph(graph, "Resources")
+		printGraph(graph, "Outputs")
 	},
 }
 
 func init() {
-	// Disabled for now
-	// rootCmd.AddCommand(graphCmd)
+	graphCmd.Flags().BoolVarP(&allLinks, "all", "a", false, "Display all elements, even those without any dependencies")
+	rootCmd.AddCommand(graphCmd)
 }
