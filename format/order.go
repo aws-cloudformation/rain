0d43bbddc2d1a7c4fa4e457521b506c04c3ecb2c
Steve Engledow <steve@offend.me.uk>
2019-07-02 15:33:25 +0100

    Fix bug in property naming

--- format/order.go
+++ format/order.go
@@ -57,7 +57,7 @@ func ordering(path []interface{}) []string {
 			"NotResource",
 			"Condition",
 		}
-	case len(path) > 2 && path[0] == "Resources" && path[1] == "Properties":
+	case len(path) > 3 && path[0] == "Resources" && path[2] == "Properties":
 		return []string{
 			"Name",
 			"Description",
