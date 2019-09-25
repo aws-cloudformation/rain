9cbcbfe674089d263ce6b0531d720c4261e5edfa
Steve Engledow <steve@offend.me.uk>
2019-09-25 11:43:08 -0700

    Generate README and add link to documentation site

--- docs/generate.go
+++ docs/generate.go
@@ -3,11 +3,37 @@ package main
 //go:generate go run generate.go
 
 import (
+	"bytes"
+	"io/ioutil"
 	"os"
+	"strings"
+	"text/template"
 
 	"github.com/aws-cloudformation/rain/cmd"
 	"github.com/spf13/cobra/doc"
 )
+
+var tmpl *template.Template
+
+func init() {
+	var err error
+
+	tmpl = template.New("README.tmpl")
+
+	tmpl = tmpl.Funcs(template.FuncMap{
+		"pad": func(s string, n int) string {
+			return strings.Repeat(" ", n-len(s))
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	tmpl, err = tmpl.ParseFiles("README.tmpl")
+	if err != nil {
+		panic(err)
+	}
+}
 
 func emptyStr(s string) string {
 	return ""
@@ -31,4 +57,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	buf := bytes.Buffer{}
+	err = tmpl.Execute(&buf, cmd.Root)
+	if err != nil {
+		panic(err)
+	}
+
+	ioutil.WriteFile("../README.md", buf.Bytes(), 0644)
 }
