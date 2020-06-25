0a031eb717039c4f0b588b54a5e42f251a74c1e2
Steve Engledow <sengledo@amazon.co.uk>
2020-06-25 15:30:22 +0100

    Fix comment colouring on YAML lines with indented values

--- console/text/colourise/yaml.go
+++ console/text/colourise/yaml.go
@@ -12,14 +12,14 @@ func Yaml(in string) string {
 	parts := strings.Split(in, "\n")
 
 	for i := range parts {
-		line := strings.Split(parts[i], ": ")
-		if len(line) == 2 {
-			parts[i] = fmt.Sprintf("%s: %s", line[0], text.Yellow(line[1]))
-		}
-
-		line = strings.Split(parts[i], "  # ")
+		line := strings.Split(parts[i], "  # ")
 		if len(line) == 2 {
 			parts[i] = fmt.Sprintf("%s  %s", line[0], text.Grey("# "+line[1]))
+		}
+
+		line = strings.Split(parts[i], ": ")
+		if len(line) == 2 {
+			parts[i] = fmt.Sprintf("%s: %s", line[0], text.Yellow(line[1]))
 		}
 	}
 
