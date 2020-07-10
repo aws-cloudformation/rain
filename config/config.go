7a26092e794b83997cb4b190976e9e0b1a1728af
Steve Engledow <sengledo@amazon.co.uk>
2020-07-10 15:47:56 +0100

    Remove config file use

--- config/config.go
+++ config/config.go
@@ -2,16 +2,8 @@ package config
 
 import (
 	"fmt"
-	"os"
-	"path"
 
 	"github.com/aws-cloudformation/rain/console/text"
-	"github.com/spf13/viper"
-)
-
-const (
-	configName = "config"
-	configDir  = ".rain"
 )
 
 // Debug defines whether debug mode is enabled
@@ -27,43 +19,5 @@ var Region = ""
 func Debugf(message string, parts ...interface{}) {
 	if Debug {
 		fmt.Println(text.Orange("DEBUG: " + fmt.Sprintf(message, parts...)))
-	}
-}
-
-func init() {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		// No place like home
-		panic(err)
-	}
-
-	viper.SetConfigName(configName)
-	viper.AddConfigPath(path.Join(home, configDir))
-
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
-			panic(fmt.Errorf("Failed to read config file: %s", err.Error()))
-		} else {
-			Debugf("Creating new config file")
-
-			err = os.Mkdir(path.Join(home, configDir), 0700)
-			if err != nil && !os.IsExist(err) {
-				panic(fmt.Errorf("Failed to create config directory: %s", err.Error()))
-			}
-
-			f, err := os.Create(path.Join(home, configDir, configName) + ".yaml")
-			if err != nil {
-				panic(fmt.Errorf("Failed to create config file: %s", err.Error()))
-			}
-			f.Chmod(0600)
-			f.Close()
-
-			err = viper.WriteConfig()
-			if err != nil {
-				panic(fmt.Errorf("Failed to write config file: %s", err.Error()))
-			}
-		}
-	} else {
-		Debugf("Loaded config file")
 	}
 }
--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/google/go-cmp v0.4.0
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/pflag v1.0.5
-	github.com/spf13/viper v1.7.0
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776
 )
 
