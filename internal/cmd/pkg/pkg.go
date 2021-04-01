5caefc654b72628bbce0a6e949cc3b6ccd31f44c
Steve Engledow <sengledo@amazon.co.uk>
2021-04-01 15:31:58 +0100

    Improve help string for pkg command

--- internal/cmd/pkg/pkg.go
+++ internal/cmd/pkg/pkg.go
@@ -12,9 +12,32 @@ import (
 
 // Cmd is the merge command's entrypoint
 var Cmd = &cobra.Command{
-	Use:                   "pkg <template>",
-	Short:                 "Package local artifacts into the template through Rain:: directives",
-	Long:                  "Package local artifacts into the template through Rain:: directives",
+	Use:   "pkg <template>",
+	Short: "Package local artifacts into a template",
+	Long: `Performs the same functions as "aws cloudformation package" but with added functionality.
+
+You may use the following, rain-specific directives in templates packaged with "rain pkg":
+
+  !Rain::Embed <path>          Embeds the contents of the file at <path> into the template as a string
+
+  !Rain::Include <path>        Reads the file at <path> as YAML/JSON and inserts the resulting object into the template
+
+  !Rain::S3Http <path>         Uploads <path> (zipping first if it is a directory) to S3
+                               and embeds the S3 HTTP URL into the template as a string
+
+  !Rain::S3 <path>             Uploads <path> (zipping first if it is a directory) to S3
+                               and embeds the S3 URI into the template as a string
+
+  !Rain::S3 <object>           supply an object with the following properties: 
+    Path: <path>               a file or directory to be uploaded to S3
+    Zip: true|false            If "true", rain with zip <path> even if it is a file
+    BucketProperty: <bucket>   If you supply "BucketProperty" and "KeyProperty", rain pkg will
+    KeyProperty: <key>         include the uploaded file/directory's details as an object in the template
+                               with the property names you specify.
+    Format: Uri|Http           Specify which format rain pkg should return the S3 location as.
+                               Do not specify this property if you supply BucketProperty and KeyProperty.
+                               The default Format is "Uri".
+`,
 	Args:                  cobra.ExactArgs(1),
 	DisableFlagsInUseLine: true,
 	Run: func(cmd *cobra.Command, args []string) {
