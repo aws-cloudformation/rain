9bfc0c47921b33667ad03b5d1856af5f8a664550
Eric Beard <ezbeard@amazon.com>
2023-01-31 16:16:55 -0800

    Forecast integ templates

--- internal/cmd/forecast/forecast_integ.sh
+++ internal/cmd/forecast/forecast_integ.sh
@@ -7,16 +7,24 @@
 # go build ./cmd/rain
 # ./internal/cmd/forecast/forecast_integ.sh
 
+set -x
+
 # First, run forecast on a template that has not been deployed, 
 # to test the ability to create the stack
-create_result=$(./rain forecast test/templates/forecast.yml forecast-integ)
+create_result=$(./rain forecast test/templates/forecast-fail.yml forecast-integ)
+
+echo $create_result | grep "2 checks failed out of 8 total checks"
+echo "Result: $?"
 
 # Deploy the stack, then forecast again to check updates and deletes
-./rain deploy test/templates/forecast.yml forecast-integ
-update_result=$(./rain forecast test/templates/forecast.yml forecast-integ)
+./rain deploy test/templates/forecast-succeed.yml forecast-integ -y
+
+update_result=$(./rain forecast test/templates/forecast-succeed.yml forecast-integ)
+echo $create_result | grep "All 5 checks passed"
+echo "Result: $?"
 
 # Delete the stack
-./rain delete forecast-integ
+./rain delete forecast-integ -y
 
 # TODO - Test with various roles to make sure it correctly predicts auth failures
 # Roles will need to be created ahead of time ("Admin", "ReadOnly")
