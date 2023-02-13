cb6f4aaa653f9e0269be8e877cd4696dbdcbce69
Eric Beard <ezbeard@amazon.com>
2023-02-13 11:24:27 -0800

    Fail sooner when region is not specified

--- internal/aws/aws.go
+++ internal/aws/aws.go
@@ -87,6 +87,10 @@ func loadConfig(ctx context.Context, sessionName string) *aws.Config {
 		panic(errors.New("could not establish AWS credentials; please run 'aws configure' or choose a profile"))
 	}
 
+	if cfg.Region == "" {
+		panic(errors.New("a region was not specified. You can run 'aws configure' or choose a profile with a region"))
+	}
+
 	return &cfg
 }
 
