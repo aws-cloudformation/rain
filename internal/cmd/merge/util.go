6362632a8f5ed6595255bb14ce7148616f21bde9
Yuta Kitamura <kitayu@amazon.co.jp>
2023-06-12 00:07:44 +0900

    Make AWS::CloudFormation::Interface metadata mergeable

--- internal/cmd/merge/util.go
+++ internal/cmd/merge/util.go
@@ -61,6 +61,51 @@ func mergeTemplates(dstTemplate, srcTemplate cft.Template) (cft.Template, error)
 
 				dst[key] = append(dst[key].([]interface{}), src[key])
 			}
+
+		case "Metadata": // Combine metadata
+			dstMap := dst[key].(map[string]interface{})
+			srcMap := src[key].(map[string]interface{})
+			for k, _ := range srcMap {
+				if _, ok := dstMap[k]; !ok {
+					dstMap[k] = srcMap[k]
+				} else {
+					if k == "AWS::CloudFormation::Interface" {
+						dstInterface := dstMap[k].(map[string]interface{})
+						srcInterface := srcMap[k].(map[string]interface{})
+
+						// Concatenate ParameterGroups
+						if srcParameterGroups, ok := srcInterface["ParameterGroups"].([]interface{}); ok {
+							dstParameterGroups, ok := dstInterface["ParameterGroups"].([]interface{})
+							if !ok {
+								dstParameterGroups = srcParameterGroups
+							} else {
+								dstParameterGroups = append(dstParameterGroups, srcParameterGroups...)
+							}
+							dstInterface["ParameterGroups"] = dstParameterGroups
+						}
+						// Combine ParameterLabels
+						if _, ok := srcInterface["ParameterLabels"]; ok {
+							if err := checkMerge("ParameterLabels", dstInterface, srcInterface); err != nil {
+								return cft.Template{}, err
+							}
+						}
+					} else {
+						if forceMerge {
+							for i := 2; true; i++ {
+								newKey := fmt.Sprintf("%s_%d", k, i)
+								if _, ok := dstMap[newKey]; !ok {
+									k = newKey
+									break
+								}
+							}
+						} else {
+							return cft.Template{}, fmt.Errorf("templates have clashing %s: %s", key, k)
+						}
+						dstMap[k] = value
+					}
+				}
+			}
+
 		default:
 			err := checkMerge(key, dst, src)
 			if err != nil {
