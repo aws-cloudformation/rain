baf611a927e21bc777dde87692e056970506a80b
Yuta Kitamura <kitayu@amazon.co.jp>
2023-06-14 15:20:08 +0900

    Add unit-test merge with empty template

--- internal/cmd/merge/merge_test.go
+++ internal/cmd/merge/merge_test.go
@@ -58,7 +58,6 @@ func TestMergeTemplatesSuccess(t *testing.T) {
 					},
 				},
 			},
-
 			"Baz": "quux",
 		},
 		"Parameters": map[string]interface{}{
@@ -136,6 +135,81 @@ func TestMergeTemplatesSuccess(t *testing.T) {
 	if d := cmp.Diff(actual.Map(), expected.Map()); d != "" {
 		t.Errorf(d)
 	}
+}
+
+func TestEmptyMergeTemplatesSuccess(t *testing.T) {
+	src, _ := parse.Map(map[string]interface{}{
+		"AWSTemplateFormatVersion": "ok to overwrite",
+		"Description":              "Line 2",
+		"Metadata": map[string]interface{}{
+			"AWS::CloudFormation::Interface": map[string]interface{}{
+				"ParameterGroups": []interface{}{
+					map[string]interface{}{
+						"Label": map[string]interface{}{
+							"default": "Amazon EC2 Configuration",
+						},
+						"Parameters": []interface{}{
+							"InstanceType",
+							"KeyName",
+						},
+					},
+				},
+				"ParameterLabels": map[string]interface{}{
+					"KeyName": map[string]interface{}{
+						"default": "EC2 Instance Ker Pair",
+					},
+				},
+			},
+
+			"Baz": "quux",
+		},
+		"Parameters": map[string]interface{}{
+			"Name": map[string]interface{}{
+				"Type": "String",
+			},
+		},
+		"Transform": map[string]interface{}{
+			"Name": "AWS::Include",
+			"Parameters": map[string]interface{}{
+				"Location": "Somewhere",
+			},
+		},
+		"Resources": map[string]interface{}{
+			"Type": "AWS::SSM::Parameter",
+			"Properties": map[string]interface{}{
+				"Name":  "test",
+				"Type":  "String",
+				"Value": "Value",
+			},
+		},
+	})
+
+	empty, _ := parse.Map(map[string]interface{}{})
+
+	// rain merge src.yaml /dev/null
+	{
+		actual, err := mergeTemplates(src, empty)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if d := cmp.Diff(actual.Map(), src.Map()); d != "" {
+			t.Errorf(d)
+		}
+	}
+
+	// rain merge /dev/null src.yaml
+	{
+		actual, err := mergeTemplates(empty, src)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if d := cmp.Diff(actual.Map(), src.Map()); d != "" {
+			t.Errorf(d)
+		}
+	}
+
 }
 
 func TestMergeTemplatesClash(t *testing.T) {
