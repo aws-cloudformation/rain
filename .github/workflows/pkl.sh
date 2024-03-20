eeb1ef05815e573d463aae2b20f36eb7852054bb
Eric Beard <ericzbeard@gmail.com>
2024-03-20 10:14:13 -0700

    Making pkl part of rain release

--- .github/workflows/pkl.sh
+++ .github/workflows/pkl.sh
@@ -4,9 +4,17 @@
 
 set -eoux pipefail
 
-FIXED_REF=${GITHUB_REF##*/}
+
+# refs/tags/v1.8.2-test1
 echo "GITHUB_REF: ${GITHUB_REF}"
+
+FIXED_REF=${GITHUB_REF##*/}
+# v1.8.2-test1
 echo "FIXED_REF: ${FIXED_REF}"
+
+SEMVER=$(echo ${FIXED_REF} | sed s/^v//g)
+# 1.8.2-test1
+echo "SEMVER: ${SEMVER}"
 
 curl -L -o /tmp/pkl https://github.com/apple/pkl/releases/download/0.25.2/pkl-linux-amd64
 chmod +x /tmp/pkl
@@ -14,8 +22,8 @@ chmod +x /tmp/pkl
 
 # Modify the PklProject file
 
-cat pkl/PklProject-template| sed s/VERSION/${FIXED_REF}/g > pkl/PklProject
+cat pkl/PklProject-template| sed s/VERSION/${SEMVER}/g > pkl/PklProject
 
 /tmp/pkl project package pkl
-gh release upload "${FIXED_REF}" .out/cloudformation@${FIXED_REF}/*
+gh release upload "${FIXED_REF}" .out/cloudformation@${SEMVER}/*
 
