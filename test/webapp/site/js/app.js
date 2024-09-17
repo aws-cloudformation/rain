f48bf9cde6ee65428b6212600c41ba3a5a379dd4
Eric Beard <ericzbeard@gmail.com>
2024-09-17 15:25:05 -0700

    Moving web app to test folder

---
+++ test/webapp/site/js/app.js
@@ -0,0 +1,9 @@
+// This is a minimal example of how to handle the auth flow with Cognit and 
+// then interact with DynamoDB once the user is logged in.
+
+
+(async function main() {
+
+    // TODO
+    alert("main") 
+})()
